65f9668485adfe8a2527198d0e0ac55e4c63efe4
ridiculousfish <corydoras@ridiculousfish.com>
2008-01-02 18:44:15 +0000

    More Tiger building stuff

--- framework/sources/HFTypes.h
+++ framework/sources/HFTypes.h
@@ -6,6 +6,9 @@ typedef unsigned long NSUInteger;
 typedef int NSInteger;
 typedef unsigned int NSUInteger;
 #endif
+#define NSIntegerMax    LONG_MAX
+#define NSIntegerMin    LONG_MIN
+#define NSUIntegerMax   ULONG_MAX
 #define NSINTEGER_DEFINED 1
 #endif
 
