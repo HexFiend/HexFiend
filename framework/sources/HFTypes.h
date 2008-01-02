f830871d4fb19cd62aa0baef30a0cab6a193f0f0
ridiculousfish <corydoras@ridiculousfish.com>
2008-01-02 18:34:47 +0000

    Defined NSInteger for Tiger

--- framework/sources/HFTypes.h
+++ framework/sources/HFTypes.h
@@ -1,3 +1,12 @@
+#ifndef NSINTEGER_DEFINED
+#if __LP64__ || NS_BUILD_32_LIKE_64
+typedef long NSInteger;
+typedef unsigned long NSUInteger;
+#else
+typedef int NSInteger;
+typedef unsigned int NSUInteger;
+#endif
+#endif
 
 /* A 64 bit range type we use pervasively */
 typedef struct {
