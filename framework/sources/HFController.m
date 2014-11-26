HexFiend/HexFiend

commit 34dd9ac8d6d5892b5147b75743be2dca73964635

misc modernization

framework/sources/HFController.m
@@ -100,7 +100,7 @@ static inline Class preferredByteArrayClass(void) {
     _hfflags.showcallouts = YES;
     _hfflags.selectable = YES;
     representers = [[NSMutableArray alloc] init];
-    [self setFont:[NSFont fontWithName:HFDEFAULT_FONT size:HFDEFAULT_FONTSIZE]];
+    _font = [[NSFont fontWithName:HFDEFAULT_FONT size:HFDEFAULT_FONTSIZE] retain];
     return self;
 }
 
@@ -113,7 +113,7 @@ static inline Class preferredByteArrayClass(void) {
     [undoOperations release];
     [undoManager release];
     [undoCoalescer release];
-    [font release];
+    [_font release];
     [byteArray removeObserver:self forKeyPath:@"changesAreLocked"];
     [byteArray release];
     [cachedData release];
@@ -126,7 +126,7 @@ static inline Class preferredByteArrayClass(void) {
     [coder encodeObject:representers forKey:@"HFRepresenters"];
     [coder encodeInt64:bytesPerLine forKey:@"HFBytesPerLine"];
     [coder encodeInt64:bytesPerColumn forKey:@"HFBytesPerColumn"];
-    [coder encodeObject:font forKey:@"HFFont"];
+    [coder encodeObject:_font forKey:@"HFFont"];
     [coder encodeDouble:lineHeight forKey:@"HFLineHeight"];
     [coder encodeBool:_hfflags.antialias forKey:@"HFAntialias"];
     [coder encodeBool:_hfflags.colorbytes forKey:@"HFColorBytes"];
@@ -143,7 +143,7 @@ static inline Class preferredByteArrayClass(void) {
     [self _sharedInit];
     bytesPerLine = (NSUInteger)[coder decodeInt64ForKey:@"HFBytesPerLine"];
     bytesPerColumn = (NSUInteger)[coder decodeInt64ForKey:@"HFBytesPerColumn"];
-    font = [[coder decodeObjectForKey:@"HFFont"] retain];
+    _font = [[coder decodeObjectForKey:@"HFFont"] retain];
     lineHeight = (CGFloat)[coder decodeDoubleForKey:@"HFLineHeight"];
     _hfflags.antialias = [coder decodeBoolForKey:@"HFAntialias"];
     _hfflags.colorbytes = [coder decodeBoolForKey:@"HFColorBytes"];
@@ -163,7 +163,7 @@ static inline Class preferredByteArrayClass(void) {
 }
 
 - (NSArray *)representers {
-    return [NSArray arrayWithArray:representers];
+    return [representers copy];
 }
 
 - (void)notifyRepresentersOfChanges:(HFControllerPropertyBits)bits {
@@ -299,19 +299,15 @@ static inline Class preferredByteArrayClass(void) {
     return lineHeight;
 }
 
-- (NSFont *)font {
-    return font;
-}
-
 - (void)setFont:(NSFont *)val {
-    if (val != font) {
+    if (val != _font) {
         CGFloat priorLineHeight = [self lineHeight];
         
-        [font release];
-        font = [val copy];
+        [_font release];
+        _font = [val copy];
         
         NSLayoutManager *manager = [[NSLayoutManager alloc] init];
-        lineHeight = [manager defaultLineHeightForFont:font];
+        lineHeight = [manager defaultLineHeightForFont:_font];
         [manager release];
         
         HFControllerPropertyBits bits = HFControllerFont;
@@ -1344,7 +1340,7 @@ static inline Class preferredByteArrayClass(void) {
     
     /* Figure out how big a word is.  By default, it's the column width, unless we have no columns, in which case it's the bytes per line. */
     NSUInteger wordGranularity = [self bytesPerColumn];
-    if (wordGranularity == 0) wordGranularity = MAX(1, [self bytesPerLine]);
+    if (wordGranularity == 0) wordGranularity = MAX(1u, [self bytesPerLine]);
     if (selectionAnchor == NO_SELECTION) {
         /* Pick the anchor inline with the choice of direction */
         if (direction == HFControllerDirectionLeft) locationToConsider = [self _minimumSelectionLocation];

HexFiend_2.xcodeproj/project.pbxproj
@@ -445,7 +445,6 @@
 		D06D60A50FA7E980002376EE /* test_btree_main.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = test_btree_main.m; sourceTree = "<group>"; };
 		D06D60BB0FA7EA23002376EE /* TreeEntry.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TreeEntry.h; sourceTree = "<group>"; };
 		D06D60BC0FA7EA23002376EE /* TreeEntry.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = TreeEntry.m; sourceTree = "<group>"; };
-		D06D60FE0FA7EB33002376EE /* BTree_Test_Prefix.pch */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = BTree_Test_Prefix.pch; sourceTree = "<group>"; };
 		D06D61EC0FA7FA7C002376EE /* HFBTreeByteArray.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = HFBTreeByteArray.h; path = sources/HFBTreeByteArray.h; sourceTree = "<group>"; };
 		D06D61ED0FA7FA7C002376EE /* HFBTreeByteArray.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = HFBTreeByteArray.m; path = sources/HFBTreeByteArray.m; sourceTree = "<group>"; };
 		D071391F146F7898004D3792 /* English */ = {isa = PBXFileReference; lastKnownFileType = file.xib; name = English; path = English.lproj/MainMenu.xib; sourceTree = "<group>"; };
@@ -982,7 +981,6 @@
 				D06D60A30FA7E980002376EE /* NaiveArray.h */,
 				D06D60A40FA7E980002376EE /* NaiveArray.m */,
 				D06D60A50FA7E980002376EE /* test_btree_main.m */,
-				D06D60FE0FA7EB33002376EE /* BTree_Test_Prefix.pch */,
 			);
 			name = BTree_Testing;
 			path = sources/BTree/BTree_Testing;
@@ -1899,7 +1897,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
-				GCC_C_LANGUAGE_STANDARD = c99;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
 				GCC_PREPROCESSOR_DEFINITIONS = "NDEBUG=1";
 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
 				GCC_WARN_ABOUT_DEPRECATED_FUNCTIONS = YES;
@@ -1990,7 +1988,7 @@
 			isa = XCBuildConfiguration;
 			buildSettings = {
 				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = framework/sources/BTree/BTree_Testing/BTree_Test_Prefix.pch;
+				GCC_PREFIX_HEADER = framework/sources/HexFiend_2_Framework_Prefix.pch;
 				INSTALL_PATH = /usr/local/bin;
 				OTHER_CFLAGS = "-DHFTEST_BTREES=1";
 				PRODUCT_NAME = Test_BTree;
@@ -2032,7 +2030,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
-				GCC_C_LANGUAGE_STANDARD = c99;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
 				GCC_OPTIMIZATION_LEVEL = 0;
 				GCC_PREPROCESSOR_DEFINITIONS = "HFUNIT_TESTS=1";
 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
@@ -2125,7 +2123,7 @@
 			isa = XCBuildConfiguration;
 			buildSettings = {
 				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = framework/sources/BTree/BTree_Testing/BTree_Test_Prefix.pch;
+				GCC_PREFIX_HEADER = framework/sources/HexFiend_2_Framework_Prefix.pch;
 				INSTALL_PATH = /usr/local/bin;
 				OTHER_CFLAGS = "-DHFTEST_BTREES=1";
 				PRODUCT_NAME = Test_BTree;
@@ -2167,7 +2165,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
-				GCC_C_LANGUAGE_STANDARD = c99;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
 				GCC_PREPROCESSOR_DEFINITIONS = "NDEBUG=1";
 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
 				GCC_WARN_ABOUT_DEPRECATED_FUNCTIONS = YES;
@@ -2260,7 +2258,7 @@
 			isa = XCBuildConfiguration;
 			buildSettings = {
 				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = framework/sources/BTree/BTree_Testing/BTree_Test_Prefix.pch;
+				GCC_PREFIX_HEADER = framework/sources/HexFiend_2_Framework_Prefix.pch;
 				INSTALL_PATH = /usr/local/bin;
 				OTHER_CFLAGS = "-DHFTEST_BTREES=1";
 				PRODUCT_NAME = Test_BTree;
@@ -2307,7 +2305,7 @@
 			isa = XCBuildConfiguration;
 			buildSettings = {
 				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = framework/sources/BTree/BTree_Testing/BTree_Test_Prefix.pch;
+				GCC_PREFIX_HEADER = framework/sources/HexFiend_2_Framework_Prefix.pch;
 				INSTALL_PATH = /usr/local/bin;
 				OTHER_CFLAGS = "-DHFTEST_BTREES=1";
 				PRODUCT_NAME = Test_BTree;
@@ -2330,7 +2328,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
-				GCC_C_LANGUAGE_STANDARD = c99;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
 				GCC_OPTIMIZATION_LEVEL = 0;
 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
 				GCC_WARN_ABOUT_DEPRECATED_FUNCTIONS = YES;
@@ -2423,7 +2421,7 @@
 			isa = XCBuildConfiguration;
 			buildSettings = {
 				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = framework/sources/BTree/BTree_Testing/BTree_Test_Prefix.pch;
+				GCC_PREFIX_HEADER = framework/sources/HexFiend_2_Framework_Prefix.pch;
 				INSTALL_PATH = /usr/local/bin;
 				OTHER_CFLAGS = "-DHFTEST_BTREES=1";
 				PRODUCT_NAME = Test_BTree;
@@ -2465,7 +2463,7 @@
 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
-				GCC_C_LANGUAGE_STANDARD = c99;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
 				GCC_OPTIMIZATION_LEVEL = 0;
 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
 				GCC_WARN_ABOUT_DEPRECATED_FUNCTIONS = YES;

HexFiend_2.xcodeproj/xcshareddata/xcschemes/Debug + CodeSign.xcscheme
@@ -61,28 +61,12 @@
       </BuildableProductRunnable>
       <CommandLineArguments>
          <CommandLineArgument
-            argument = "/Users/peter/Documents/first.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Documents/second.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
             argument = "-HFDebugMenu"
             isEnabled = "YES">
          </CommandLineArgument>
          <CommandLineArgument
             argument = "YES"
             isEnabled = "YES">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/As.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/Bs.data"
-            isEnabled = "NO">
          </CommandLineArgument>
       </CommandLineArguments>
       <AdditionalOptions>

HexFiend_2.xcodeproj/xcshareddata/xcschemes/Debug + UnitTests.xcscheme
@@ -20,6 +20,20 @@
                ReferencedContainer = "container:HexFiend_2.xcodeproj">
             </BuildableReference>
          </BuildActionEntry>
+         <BuildActionEntry
+            buildForTesting = "YES"
+            buildForRunning = "YES"
+            buildForProfiling = "YES"
+            buildForArchiving = "YES"
+            buildForAnalyzing = "YES">
+            <BuildableReference
+               BuildableIdentifier = "primary"
+               BlueprintIdentifier = "9408FBF919DD72DE004F800E"
+               BuildableName = "HexFiend_Framework Tests.xctest"
+               BlueprintName = "HexFiend_Framework Tests"
+               ReferencedContainer = "container:HexFiend_2.xcodeproj">
+            </BuildableReference>
+         </BuildActionEntry>
       </BuildActionEntries>
    </BuildAction>
    <TestAction
@@ -61,28 +75,12 @@
       </BuildableProductRunnable>
       <CommandLineArguments>
          <CommandLineArgument
-            argument = "/Users/peter/Documents/first.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Documents/second.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
             argument = "-HFDebugMenu"
             isEnabled = "YES">
          </CommandLineArgument>
          <CommandLineArgument
             argument = "YES"
             isEnabled = "YES">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/As.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/Bs.data"
-            isEnabled = "NO">
          </CommandLineArgument>
       </CommandLineArguments>
       <AdditionalOptions>

HexFiend_2.xcodeproj/xcshareddata/xcschemes/Debug.xcscheme
@@ -61,14 +61,6 @@
       </BuildableProductRunnable>
       <CommandLineArguments>
          <CommandLineArgument
-            argument = "/Users/peter/Documents/first.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Documents/second.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
             argument = "-HFDebugMenu"
             isEnabled = "YES">
          </CommandLineArgument>
@@ -76,27 +68,7 @@
             argument = "YES"
             isEnabled = "YES">
          </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/As.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
-         <CommandLineArgument
-            argument = "/Users/peter/Desktop/Bs.data"
-            isEnabled = "NO">
-         </CommandLineArgument>
       </CommandLineArguments>
-      <EnvironmentVariables>
-         <EnvironmentVariable
-            key = "NSZombieEnabled"
-            value = "YES"
-            isEnabled = "NO">
-         </EnvironmentVariable>
-         <EnvironmentVariable
-            key = "MallocStackLoggingNoCompact"
-            value = "YES"
-            isEnabled = "NO">
-         </EnvironmentVariable>
-      </EnvironmentVariables>
       <AdditionalOptions>
          <AdditionalOption
             key = "DYLD_INSERT_LIBRARIES"

HexFiend_2.xcodeproj/xcshareddata/xcschemes/Release.xcscheme
@@ -41,7 +41,7 @@
    </TestAction>
    <LaunchAction
       selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
-      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.GDB"
+      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
       displayScaleIsEnabled = "NO"
       displayScale = "1.00"
       launchStyle = "0"

app/sources/BaseDataDocument.m
@@ -1688,7 +1688,7 @@ cancelled:;
     if ([ranges count] > 0) {
         HFRange range = [ranges[0] HFRange];
         /* We always set a bookmark on at least one byte */
-        range.length = MAX(range.length, 1);
+        range.length = MAX(range.length, 1u);
         NSIndexSet *usedBookmarks = [controller bookmarksInRange:HFRangeMake(0, [controller contentsLength])];
         
         /* Find the first index that bookmarks does not contain, excepting 0 */

app/sources/DiffDocument.h
@@ -7,8 +7,6 @@
 @interface DiffDocument : BaseDataDocument {
     HFByteArray *leftBytes, *rightBytes;
     HFByteArrayEditScript *editScript;
-    NSString *leftFileName;
-    NSString *rightFileName;
     IBOutlet HFTextView *leftTextView;
     IBOutlet HFTextView *rightTextView;
     IBOutlet NSTableView *diffTable;

commit 710c9852ba29bdd4420b332ef24dec222d7cf455

Fixed lineHeight not being set due to missing call to setFont (regression from 34dd9ac)

framework/sources/HFController.m
@@ -100,7 +100,7 @@ static inline Class preferredByteArrayClass(void) {
     _hfflags.showcallouts = YES;
     _hfflags.selectable = YES;
     representers = [[NSMutableArray alloc] init];
-    _font = [[NSFont fontWithName:HFDEFAULT_FONT size:HFDEFAULT_FONTSIZE] retain];
+    [self setFont:[NSFont fontWithName:HFDEFAULT_FONT size:HFDEFAULT_FONTSIZE]];
     return self;
 }
 