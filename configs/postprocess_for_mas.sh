119d964e7c0b11cf7bde705a32ff6dd033d44d61
ridiculousfish <corydoras@ridiculousfish.com>
2017-01-08 02:37:08 -0800

    Delete the privileged helper tool from Mac App Store builds
    Use the same script we use to erase Sparkle to also delete
    Hex Fiend's privileged helper binary from the package.
    The Mac App Store does not support our privileged helper
    due to sandboxing.

--- configs/postprocess_for_mas.sh
+++ configs/postprocess_for_mas.sh
@@ -11,10 +11,16 @@ set -e
 die() { echo "Error: $@" 1>&2 ; exit 1; }
 
 test "${BUILD_FOR_MAS}" = "1" || exit 0
-
 test -d "${BUILT_PRODUCTS_DIR}" || die 'Missing $BUILT_PRODUCTS_DIR'
 test -n "${WRAPPER_NAME}" || die 'Missing $WRAPPER_NAME'
 
-apppath="${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"
-sparklepath="${apppath}"/Contents/Frameworks/Sparkle.framework
-rm -r "${sparklepath}" && echo "Removed ${sparklepath}"
+
+erase() {
+  FULLPATH="${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}$1"
+  rm -r "${FULLPATH}" && echo "Removed ${FULLPATH}";
+}
+
+erase /Contents/Frameworks/Sparkle.framework
+
+# Currently PrivilegedHelper is the only thing under the Library dir
+erase /Contents/Library
