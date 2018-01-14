c87ec767cc1eab5ec34253886e88cf6246570000
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-01-14 11:57:35 -0800

    Load templates from directory into popup

--- app/sources/HFBinaryTemplateController.m
+++ app/sources/HFBinaryTemplateController.m
@@ -9,15 +9,67 @@
 #import "HFBinaryTemplateController.h"
 #import "HFTemplateNode.h"
 
+@interface HFTemplateFile : NSObject
+
+@property (copy) NSString *path;
+@property (copy) NSString *name;
+
+@end
+
+@implementation HFTemplateFile
+
+@end
+
 @interface HFBinaryTemplateController () <NSTableViewDataSource, NSTableViewDelegate>
 
 @property (weak) IBOutlet NSTableView *tableView;
 @property (weak) IBOutlet NSTextField *errorTextField;
+@property (weak) IBOutlet NSPopUpButton *templatesPopUp;
+
 @property HFTemplateNode *node;
+@property NSArray<HFTemplateFile*> *templates;
 
 @end
 
 @implementation HFBinaryTemplateController
+
+- (void)loadTemplates:(id __unused)sender {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSString *dir = [[NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:[NSBundle mainBundle].bundleIdentifier] stringByAppendingPathComponent:@"Templates"];
+    NSMutableArray<HFTemplateFile*> *templates = [NSMutableArray array];
+    for (NSString *filename in [fm contentsOfDirectoryAtPath:dir error:nil]) {
+        if ([filename.pathExtension isEqualToString:@"tcl"]) {
+            HFTemplateFile *file = [[HFTemplateFile alloc] init];
+            file.path = [dir stringByAppendingPathComponent:filename];
+            file.name = [filename stringByDeletingPathExtension];
+            [templates addObject:file];
+        }
+    }
+    [templates sortUsingDescriptors:@[[[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES]]];
+    [self.templatesPopUp removeAllItems];
+    NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"None", nil) action:@selector(noTemplate:) keyEquivalent:@""];
+    item.target = self;
+    [self.templatesPopUp.menu addItem:item];
+    [self.templatesPopUp.menu addItem:[NSMenuItem separatorItem]];
+    if (templates.count > 0) {
+        for (HFTemplateFile *file in templates) {
+            [self.templatesPopUp addItemWithTitle:file.name];
+        }
+        [self.templatesPopUp.menu addItem:[NSMenuItem separatorItem]];
+    }
+    item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"Refresh", nil) action:@selector(loadTemplates:) keyEquivalent:@""];
+    item.target = self;
+    [self.templatesPopUp.menu addItem:item];
+    self.templates = templates;
+}
+
+- (void)awakeFromNib {
+    [self loadTemplates:self];
+}
+
+- (void)noTemplate:(id __unused)sender {
+    
+}
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView * __unused)tableView {
     return self.node.children.count;
