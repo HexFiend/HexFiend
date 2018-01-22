cfb5fced22c304b9952423418d11f9d4d9763794
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-01-21 18:12:42 -0800

    Implement reference lists

--- templates/Apple/ResourceFork.tcl
+++ templates/Apple/ResourceFork.tcl
@@ -22,12 +22,28 @@ section "Map" {
     uint16 "Attributes"
     uint16 "Type List Offset"
     uint16 "Name List Offset"
-    set num_types [uint16 "Num Types (Minus 1)"]
+    set num_types [uint16 "Num Types - 1"]
+    set total_num_resources 0
     section "Type List" {
-        for {set i 0} {$i < [expr {$num_types+1}]} {incr i} {
-            ascii 4 "Type"
-            uint16 "Num Resources (Minus 1)"
-            uint16 "Type List Offset"
+        for {set i 0} {$i < [expr {$num_types + 1}]} {incr i} {
+            section [expr $i + 1] {
+                ascii 4 "Type"
+                set num_resources [uint16 "Num Resources - 1"]
+                uint16 "Type List Offset"
+                incr total_num_resources [expr {$num_resources + 1}]
+            }
+        }
+    }
+    section "Reference Lists" {
+        for {set i 0} {$i < $total_num_resources} {incr i} {
+            section [expr $i + 1] {
+                uint16 "Resource ID"
+                uint16 "Name List Offset"
+                uint8 "Attributes"
+                hex 3 "Data Offset"
+                #uint24 "Data Offset"
+                uint32 "Handle"
+            }
         }
     }
 }
