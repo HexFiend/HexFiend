e880e1e251e83170ba2300edc7d8fc776ec11e60
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-07-01 20:07:44 -0700

    Show toc compressed bytes

--- templates/Archives/XAR.tcl
+++ templates/Archives/XAR.tcl
@@ -3,6 +3,7 @@ requires 0 "78617221" ;# xar!
 ascii 4 "Signature"
 uint16 "Header Size"
 uint16 "Version"
-uint64 "TOC Compressed Length"
+set compressed_length [uint64 "TOC Compressed Length"]
 uint64 "TOC Uncompressed Length"
 uint32 "Checksum"
+bytes $compressed_length "TOC Compressed"
