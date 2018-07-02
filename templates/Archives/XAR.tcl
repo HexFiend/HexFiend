12f5db36b100202efc9441ab3cee5a7c31cdfc80
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-07-01 19:43:01 -0700

    Start on xar template

---
+++ templates/Archives/XAR.tcl
@@ -0,0 +1,8 @@
+big_endian
+requires 0 "78617221" ;# xar!
+ascii 4 "Signature"
+uint16 "Header Size"
+uint16 "Version"
+uint64 "TOC Compressed Length"
+uint64 "TOC Uncompressed Length"
+uint32 "Checksum"
