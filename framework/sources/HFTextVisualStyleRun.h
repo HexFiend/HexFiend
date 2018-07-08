a46e2ac40118d3f19c56d51e150b19d6b7c5d278
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-07-07 23:36:07 -0700

    Compile HFTextVisualStyleRun for iOS

--- framework/sources/HFTextVisualStyleRun.h
+++ framework/sources/HFTextVisualStyleRun.h
@@ -5,10 +5,15 @@
 //  Copyright 2009 ridiculous_fish. All rights reserved.
 //
 
-@interface HFTextVisualStyleRun : NSObject {}
+@interface HFTextVisualStyleRun : NSObject
 
+#if TARGET_OS_IPHONE
+@property (nonatomic, copy) UIColor *foregroundColor;
+@property (nonatomic, copy) UIColor *backgroundColor;
+#else
 @property (nonatomic, copy) NSColor *foregroundColor;
 @property (nonatomic, copy) NSColor *backgroundColor;
+#endif
 @property (nonatomic) NSRange range;
 @property (nonatomic) BOOL shouldDraw;
 @property (nonatomic) CGFloat scale;
