83265b1cc4e8936c2cacdb40a44bd7372e2349db
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-07-28 17:22:46 -0700

    Show usage instead of specific errors

--- hexf/main.m
+++ hexf/main.m
@@ -83,25 +83,18 @@ static NSString *kAppIdentifier = @"com.ridiculousfish.HexFiend";
         NSString *arg = [args objectAtIndex:i];
         if ([arg hasPrefix:@"-"]) {
             if ([arg isEqualToString:@"-d"] || [arg isEqualToString:@"--diff"]) {
-                if (i == (argsCount - 1)) {
-                    fprintf(stderr, "Missing argument for \"%s\"\n", arg.UTF8String);
-                    return EXIT_FAILURE;
-                }
-                if (diffFile) {
-                    fprintf(stderr, "Argument \"%s\" can only be specified once.\n", arg.UTF8String);
-                    return EXIT_FAILURE;
+                if (i == (argsCount - 1) || diffFile) {
+                    return [self printUsage];
                 }
                 diffFile = [args objectAtIndex:i + 1];
                 i++;
                 continue;
             } else {
-                fprintf(stderr, "Unknown argument \"%s\".\n", arg.UTF8String);
-                return EXIT_FAILURE;
+                return [self printUsage];
             }
         }
         if (diffFile && filesToOpen.count == 1) {
-            fprintf(stderr, "Only one file can be specified when diff argument is used.\n");
-            return EXIT_FAILURE;
+            return [self printUsage];
         }
         NSURL *url = [NSURL fileURLWithPath:arg];
         NSString *path = url.path; // get absolute path
