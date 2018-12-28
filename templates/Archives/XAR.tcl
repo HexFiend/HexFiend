ad4c4d63a4906c966b32d8fe8e6a683fb7085b3e
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-12-27 17:32:24 -0800

    Decompress and parse table of contents, list files

--- templates/Archives/XAR.tcl
+++ templates/Archives/XAR.tcl
@@ -6,4 +6,24 @@ uint16 "Version"
 set compressed_length [uint64 "TOC Compressed Length"]
 uint64 "TOC Uncompressed Length"
 uint32 "Checksum"
-bytes $compressed_length "TOC Compressed"
+set compressed_data [bytes $compressed_length "TOC Data"]
+set xml [zlib_uncompress $compressed_data]
+
+#puts "${xml}"
+package require tdom
+set doc [dom parse $xml]
+set root [$doc documentElement]
+set nodes [$root selectNodes "//file"]
+section "Files" {
+	foreach node $nodes {
+		section [[lindex [$node getElementsByTagName "name"] 0] text] {
+			entry "ID" [$node getAttribute "id"]
+			entry "Type" [[lindex [$node getElementsByTagName "type"] 0] text]
+			entry "User" [[lindex [$node getElementsByTagName "user"] 0] text]
+			entry "Group" [[lindex [$node getElementsByTagName "group"] 0] text]
+			entry "UID" [[lindex [$node getElementsByTagName "uid"] 0] text]
+			entry "GID" [[lindex [$node getElementsByTagName "gid"] 0] text]
+			entry "Mode" [[lindex [$node getElementsByTagName "mode"] 0] text]
+		}
+	}
+}
