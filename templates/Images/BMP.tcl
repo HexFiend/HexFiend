276dfda0b5a72a9da893ad6eb5e23e9f3287a159
Matt Sephton <matt.sephton@gmail.com>
2018-12-29 20:37:37 +0000

    Add BMP.tcl (#171)
    Windows Bitmap binary template

---
+++ templates/Images/BMP.tcl
@@ -0,0 +1,35 @@
+# Reference Templates
+# https://www.sweetscape.com/010editor/repository/files/BMP.bt
+# https://raw.githubusercontent.com/synalysis/Grammars/master/bitmap.grammar
+
+# Reference Documents
+# http://www.digicamsoft.com/bmp/bmp.html
+# http://en.wikipedia.org/wiki/BMP_file_format
+
+little_endian
+requires 0 "42 4D"
+section "Header" {
+	ascii 2 "bfType"
+	uint32 "bfSize"
+	uint16 "bfReserved1"
+	uint16 "bfReserved2"
+	hex 4 "bfOffBits"
+}
+section "BITMAPINFOHEADER" {
+	uint32 "biSize"
+	uint32 "biWidth"
+	int32 "biHeight"	; # signed; heights can be negative
+	uint8 "biPlanes"
+	uint8 "biBitCount"
+	uint32 "biCompression"
+	uint32 "biSizeImage"
+	uint32 "biXPelsPerMeter"
+	uint32 "biYPelsPerMeter"
+	uint32 "biClrUsed"
+	uint32 "biClrImportant"
+}
+# section "ImageData" {
+# 	for {set i 0} {![end]} {incr i} {
+# 		uint8 "Byte $i"
+# 	}
+# }
