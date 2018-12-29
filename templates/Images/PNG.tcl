14a4821f60a92d5ab61990125b34cabc3659f3dd
Kevin Wojniak <kainjow@users.noreply.github.com>
2018-12-29 12:40:51 -0800

    spaces to tabs

--- templates/Images/PNG.tcl
+++ templates/Images/PNG.tcl
@@ -2,12 +2,12 @@ big_endian
 requires 0 "89 50 4E 47 0D 0A 1A 0A"
 hex 8 "Signature"
 while {![end]} {
-    set chunk_len [uint32]
-    set chunk_type [ascii 4]
-    move -8
-    section "Chunk $chunk_type" {
-    	uint32 "Length"
-    	ascii 4 "Type"
+	set chunk_len [uint32]
+	set chunk_type [ascii 4]
+	move -8
+	section "Chunk $chunk_type" {
+		uint32 "Length"
+		ascii 4 "Type"
 		if {$chunk_type == "IHDR"} {
 			uint32 "Width"
 			uint32 "Height"
