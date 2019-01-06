5eaed97c7a16ade661992cd564bc1af9ec496898
Kevin Wojniak <kainjow@users.noreply.github.com>
2019-01-05 19:25:55 -0800

    Use CoreText to match text alignment

--- framework/sources/BTree/HFColumnRepresenter.m
+++ framework/sources/BTree/HFColumnRepresenter.m
@@ -90,38 +90,33 @@ static const CGFloat kShadowHeight = 6;
     const NSRect bounds = self.offsetBounds;
 
     HFController *controller = self.representer.controller;
-    HFFont *font = controller.font;
-    
+    CTFontRef font = (__bridge CTFontRef)controller.font;
+    const CGFloat descent = ceil(fabs(CTFontGetDescent(font)));
     const NSUInteger bytesPerColumn = controller.bytesPerColumn;
     const NSUInteger bytesPerLine = controller.bytesPerLine;
     const CGFloat lineHeight = controller.lineHeight;
     const CGFloat horizontalContainerInset = 4; // matches what HFRepresenterTextView uses
-
-    NSMutableParagraphStyle *mutableStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    [mutableStyle setMinimumLineHeight:lineHeight];
-    [mutableStyle setMaximumLineHeight:lineHeight];
-    NSParagraphStyle *paragraphStyle = [mutableStyle copy];
-    NSDictionary *attributes = @{
-        NSFontAttributeName: font,
-        NSForegroundColorAttributeName: self.foregroundColor,
-        NSParagraphStyleAttributeName: paragraphStyle,
-    };
-    
-    NSRect drawRect = NSMakeRect(bounds.origin.x + horizontalContainerInset, bounds.origin.y + (kShadowHeight - ceil(fabs(font.descender)) - 2), 0, 0);
-    NSUInteger bytesInColumn = 0;
     const CGFloat advancement = self.glyphTable.advancement;
+
+    NSUInteger bytesInColumn = 0;
+    CGFloat x = bounds.origin.x + horizontalContainerInset;
+    CGContextRef ctx = HFGraphicsGetCurrentContext();
+    [self.foregroundColor set];
+    CGGlyph glyphs[2];
+    CGPoint positions[2];
+    positions[0].y = descent;
+    positions[1].y = descent;
+    glyphs[0] = self.glyphTable.table[0];
     for (unsigned i = 0; i < (unsigned)bytesPerLine; i++) {
-        NSString *str = [NSString stringWithFormat:@"%02X", i];
-        const NSSize strSize = [str sizeWithAttributes:attributes];
-        const NSSize drawSize = NSMakeSize(ceil(strSize.width), ceil(strSize.height));
-        drawRect.size.width = drawSize.width;
-        drawRect.size.height = drawSize.height;
-        [str drawInRect:drawRect withAttributes:attributes];
-        drawRect.origin.x += (advancement * 2);
+        glyphs[1] = self.glyphTable.table[i];
+        positions[0].x = x;
+        positions[1].x = x + advancement;
+        CTFontDrawGlyphs(font, glyphs, positions, 2, ctx);
+        x += (advancement * 2);
 
         ++bytesInColumn;
         if (bytesInColumn == bytesPerColumn) {
-            drawRect.origin.x += advancement;
+            x += advancement;
             bytesInColumn = 0;
         }
     }
