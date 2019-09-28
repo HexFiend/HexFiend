HexFiend/HexFiend

commit 0e24a4e7e717e488945c885d718dfccd2519eb49

Replace deprecated enums

framework/sources/HFController.m
@@ -989,9 +989,9 @@ static inline Class preferredByteArrayClass(void) {
     /* Determine how to perform the selection - normally, with command key, or with shift key.  Command + shift is the same as command. The shift key closes the selection - the selected range becomes the single range containing the first and last selected character. */
     _hfflags.shiftExtendSelection = NO;
     _hfflags.commandExtendSelection = NO;
-    NSUInteger flags = [event modifierFlags];
-    if (flags & NSCommandKeyMask) _hfflags.commandExtendSelection = YES;
-    else if (flags & NSShiftKeyMask) _hfflags.shiftExtendSelection = YES;
+    NSEventModifierFlags flags = [event modifierFlags];
+    if (flags & NSEventModifierFlagCommand) _hfflags.commandExtendSelection = YES;
+    else if (flags & NSEventModifierFlagCommand) _hfflags.shiftExtendSelection = YES;
     
     selectionAnchor = NO_SELECTION;
     selectionAnchorRange = HFRangeMake(NO_SELECTION, 0);
@@ -1173,7 +1173,7 @@ static inline Class preferredByteArrayClass(void) {
 #if !TARGET_OS_IPHONE
 - (void)scrollWithScrollEvent:(NSEvent *)scrollEvent {
     HFASSERT(scrollEvent != NULL);
-    HFASSERT([scrollEvent type] == NSScrollWheel);
+    HFASSERT([scrollEvent type] == NSEventTypeScrollWheel);
     CGFloat preciseScroll;
     BOOL hasPreciseScroll;
     

app/sources/AppDebugging.m
@@ -25,7 +25,7 @@ static unsigned long long unsignedLongLongValue(NSString *s) {
 - (void)installDebuggingMenuItems:(NSMenu *)menu {
     [menu addItem:[NSMenuItem separatorItem]];
     [menu addItemWithTitle:@"Show ByteArray" action:@selector(_showByteArray:) keyEquivalent:@"k"];
-    [[[menu itemArray] lastObject] setKeyEquivalentModifierMask:NSCommandKeyMask];
+    [[[menu itemArray] lastObject] setKeyEquivalentModifierMask:NSEventModifierFlagCommand];
     [menu addItemWithTitle:@"Randomly Tweak ByteArray" action:@selector(_tweakByteArray:) keyEquivalent:@""];
     [menu addItemWithTitle:@"Random ByteArray" action:@selector(_randomByteArray:) keyEquivalent:@""];
     

app/sources/AppDelegate.m
@@ -50,8 +50,8 @@
     }
 
     [NSThread detachNewThreadSelector:@selector(buildFontMenu:) toTarget:self withObject:nil];
-    [extendForwardsItem setKeyEquivalentModifierMask:[extendForwardsItem keyEquivalentModifierMask] | NSShiftKeyMask];
-    [extendBackwardsItem setKeyEquivalentModifierMask:[extendBackwardsItem keyEquivalentModifierMask] | NSShiftKeyMask];
+    [extendForwardsItem setKeyEquivalentModifierMask:[extendForwardsItem keyEquivalentModifierMask] | NSEventModifierFlagShift];
+    [extendBackwardsItem setKeyEquivalentModifierMask:[extendBackwardsItem keyEquivalentModifierMask] | NSEventModifierFlagShift];
     [extendForwardsItem setKeyEquivalent:@"]"];
     [extendBackwardsItem setKeyEquivalent:@"["];	
     [self buildEncodingMenu];

app/sources/BaseDataDocument.m
@@ -1213,7 +1213,7 @@ static inline Class preferredByteArrayClass(void) {
         saveResult = [result integerValue];
         
         /* Post an event so our event loop wakes up */
-        [NSApp postEvent:[NSEvent otherEventWithType:NSApplicationDefined location:NSZeroPoint modifierFlags:0 timestamp:0 windowNumber:0 context:NULL subtype:0 data1:0 data2:0] atStart:NO];
+        [NSApp postEvent:[NSEvent otherEventWithType:NSEventTypeApplicationDefined location:NSZeroPoint modifierFlags:0 timestamp:0 windowNumber:0 context:NULL subtype:0 data1:0 data2:0] atStart:NO];
     }];
 
     if (operationError && outError) {
@@ -1223,7 +1223,7 @@ static inline Class preferredByteArrayClass(void) {
     while ([saveView operationIsRunning]) {
         @autoreleasepool {
             @try {  
-                NSEvent *event = [NSApp nextEventMatchingMask:NSAnyEventMask untilDate:[NSDate distantFuture] inMode:NSDefaultRunLoopMode dequeue:YES];
+                NSEvent *event = [NSApp nextEventMatchingMask:NSUIntegerMax untilDate:[NSDate distantFuture] inMode:NSDefaultRunLoopMode dequeue:YES];
                 if (event) [NSApp sendEvent:event];
             }
             @catch (NSException *localException) {
@@ -1718,7 +1718,7 @@ cancelled:;
                 initWithTitle:[NSString stringWithFormat:@"Select Bookmark %lu", (unsigned long)bookmarkIndex]
                 action:@selector(selectBookmark:)
                 keyEquivalent:keString];
-        [item setKeyEquivalentModifierMask:NSCommandKeyMask];
+        [item setKeyEquivalentModifierMask:NSEventModifierFlagCommand];
         [item setAlternate:NO];
         [item setTag:bookmarkIndex];
         [items addObject:item];
@@ -1727,7 +1727,7 @@ cancelled:;
                 initWithTitle:[NSString stringWithFormat:@"Scroll to Bookmark %lu", (unsigned long)bookmarkIndex]
                 action:@selector(scrollToBookmark:)
                 keyEquivalent:keString];
-        [item setKeyEquivalentModifierMask:NSCommandKeyMask | NSShiftKeyMask];
+        [item setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagShift];
         [item setAlternate:YES];
         [item setTag:bookmarkIndex];
         [items addObject:item];

app/sources/DataInspectorPlusMinusButtonCell.m
@@ -60,7 +60,7 @@
     CGImageRef image = CGImageCreate(width, height, CHAR_BIT, bytesPerPixel * CHAR_BIT, bytesPerPixel * width, space, (CGBitmapInfo)kCGImageAlphaPremultipliedLast, provider, NULL, YES, kCGRenderingIntentDefault);
     CGDataProviderRelease(provider);
     CGColorSpaceRelease(space);
-    [[NSGraphicsContext currentContext] setCompositingOperation:NSCompositeSourceOver];
+    [[NSGraphicsContext currentContext] setCompositingOperation:NSCompositingOperationSourceOver];
     CGContextDrawImage(HFGraphicsGetCurrentContext(), *(CGRect *)&bitmapRect, image);
     CGImageRelease(image);
 }

app/sources/DataInspectorScrollView.m
@@ -20,13 +20,13 @@
     NSRect bounds = [self bounds];
     NSRect lineRect = bounds;
     lineRect.size.height = 1;
-    NSRectFillUsingOperation(NSIntersectionRect(lineRect, clipRect), NSCompositeSourceOver);
+    NSRectFillUsingOperation(NSIntersectionRect(lineRect, clipRect), NSCompositingOperationSourceOver);
 }
 
 - (void)drawRect:(NSRect)rect {
     if (!HFDarkModeEnabled()) {
         [[NSColor colorWithCalibratedWhite:(CGFloat).91 alpha:1] set];
-        NSRectFillUsingOperation(rect, NSCompositeSourceOver);
+        NSRectFillUsingOperation(rect, NSCompositingOperationSourceOver);
     }
     
     if (HFDarkModeEnabled()) {
@@ -34,7 +34,7 @@
     } else {
         [[NSColor colorWithCalibratedWhite:(CGFloat).91 alpha:1] set];
     }
-    NSRectFillUsingOperation(rect, NSCompositeSourceOver);
+    NSRectFillUsingOperation(rect, NSCompositingOperationSourceOver);
     [self drawDividerWithClip:rect];
 }
 

app/sources/DiffDocument.m
@@ -278,8 +278,8 @@ static enum DiffOverlayViewRangeType_t rangeTypeForValue(CGFloat value) {
 - (BOOL)handleEvent:(NSEvent *)event {
     BOOL handled = NO;
     BOOL frInLeftView = [self firstResponderIsInView:leftTextView], frInRightView = [self firstResponderIsInView:rightTextView];
-    NSUInteger prohibitedFlags = (NSShiftKeyMask | NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask);
-    if ([event type] == NSKeyDown && ! (prohibitedFlags & [event modifierFlags])) {
+    NSUInteger prohibitedFlags = (NSEventModifierFlagShift | NSEventModifierFlagControl | NSEventModifierFlagOption | NSEventModifierFlagCommand);
+    if ([event type] == NSEventTypeKeyDown && ! (prohibitedFlags & [event modifierFlags])) {
         if (frInLeftView || frInRightView) {
             /* Handle arrow keys */
             NSString *chars = [event characters];
@@ -295,7 +295,7 @@ static enum DiffOverlayViewRangeType_t rangeTypeForValue(CGFloat value) {
                 }
             }
         }
-    } else if ([event type] == NSScrollWheel) {
+    } else if ([event type] == NSEventTypeScrollWheel) {
         
         /* Redirect scroll wheel events to ourselves, except for those in the table (or, rather, its scroll view). If this scroll event comes very soon after the last one, then we consider it to be a momentum scroll event and direct it at the last target. */
         NSPoint location = [event locationInWindow];
@@ -1025,7 +1025,7 @@ static const CGFloat kScrollMultiplier = (CGFloat)1.5;
 
 - (void)scrollWithScrollEvent:(NSEvent *)scrollEvent {
     HFASSERT(scrollEvent != NULL);
-    HFASSERT([scrollEvent type] == NSScrollWheel);
+    HFASSERT([scrollEvent type] == NSEventTypeScrollWheel);
     long double scrollY = 0;
     
     /* Prefer precise deltas */

app/sources/DiffTextViewContainer.m
@@ -152,7 +152,7 @@
     } else {
         [[NSColor colorWithCalibratedWhite:.64 alpha:1.] set];
     }
-    NSRectFillUsingOperation(dirtyRect, NSCompositeSourceOver);
+    NSRectFillUsingOperation(dirtyRect, NSCompositingOperationSourceOver);
     
     CGContextRef ctx = HFGraphicsGetCurrentContext();
     CGFloat lineWidth = 1;

commit 937ecac8ce3866eaf43ef347b95e478697fd32ce

Fix wrong flag
Regression from 0e24a4e7e717e488945c885d718dfccd2519eb49

Closes #202

framework/sources/HFController.m
@@ -991,7 +991,7 @@ static inline Class preferredByteArrayClass(void) {
     _hfflags.commandExtendSelection = NO;
     NSEventModifierFlags flags = [event modifierFlags];
     if (flags & NSEventModifierFlagCommand) _hfflags.commandExtendSelection = YES;
-    else if (flags & NSEventModifierFlagCommand) _hfflags.shiftExtendSelection = YES;
+    else if (flags & NSEventModifierFlagShift) _hfflags.shiftExtendSelection = YES;
     
     selectionAnchor = NO_SELECTION;
     selectionAnchorRange = HFRangeMake(NO_SELECTION, 0);