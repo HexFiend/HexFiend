2807dfa824c0bae825cbf0cd79f5f81eae97d33d
Sven Dahlstrand <sven@dahlstrand.net>
2020-02-16 20:44:14 +0100

    Make sure NSMutableData is empty from start.

--- framework/sources/HFCustomEncoding.m
+++ framework/sources/HFCustomEncoding.m
@@ -136,7 +136,7 @@
 }
 
 - (NSData *)dataFromString:(NSString *)string {
-    NSMutableData *bytes = [NSMutableData dataWithLength:string.length * _bytesPerCharacter];
+    NSMutableData *bytes = [NSMutableData dataWithCapacity:string.length * _bytesPerCharacter];
     for (NSUInteger i = 0; i < string.length; ++i) {
         NSString *str = [NSString stringWithFormat:@"%C", [string characterAtIndex:i]];
         NSNumber *codepointNumber = self.stringToCharMap[str];
