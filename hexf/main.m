859004bd38744e7733a9ffb132326c5d0b3bb244
Kevin Wojniak <kainjow@users.noreply.github.com>
2020-04-30 22:29:27 -0700

    Remove stdin check
    Fixes #246

--- hexf/main.m
+++ hexf/main.m
@@ -7,7 +7,6 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#include <sys/time.h>
 
 static NSString *kAppIdentifier = @"com.ridiculousfish.HexFiend";
 
@@ -57,18 +56,7 @@ static NSString *kAppIdentifier = @"com.ridiculousfish.HexFiend";
     return YES;
 }
 
-- (BOOL)standardInputHasData {
-    fd_set rfds;
-    FD_ZERO(&rfds);
-    FD_SET(STDIN_FILENO, &rfds);
-    struct timeval timeout = {0, 0};
-    return select(1, &rfds, NULL, NULL, &timeout) == 1;
-}
-
 - (BOOL)processStandardInput {
-    if (!self.standardInputHasData) {
-        return NO;
-    }
     NSFileHandle *inFile = [NSFileHandle fileHandleWithStandardInput];
     NSData *data = [inFile readDataToEndOfFile];
     if (data.length == 0) {
