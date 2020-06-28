0ed944bbd0defc56f4b48eb0a320b3e3f30f095f
Kevin Wojniak <kainjow@users.noreply.github.com>
2020-06-27 21:30:48 -0700

    Remove unused int_memchr
    Leftover from the PowerPC implementation

--- framework/sources/HFFastMemchr.m
+++ framework/sources/HFFastMemchr.m
@@ -43,42 +43,6 @@ static unsigned char* sse_memchr(const unsigned char* haystack, unsigned char ne
 
 #endif
 
-static unsigned char* int_memchr(const unsigned char* haystack, unsigned char needle, size_t length) __attribute__ ((__noinline__, __unused__));
-static unsigned char* int_memchr(const unsigned char* haystack, unsigned char needle, size_t length) {
-    unsigned prefixLength = (unsigned)((4 - ((unsigned long)haystack) % 4) % 4);
-    unsigned suffixLength = (unsigned)(((unsigned long)(haystack + length)) % 4);
-    size_t numWords = (length - prefixLength - suffixLength) / 4;
-    
-    while (prefixLength--) {
-        if (*haystack == needle) return (unsigned char*)haystack;
-        haystack++;
-    }
-    
-    while (numWords--) {
-        unsigned val = *(unsigned int*)haystack;
-#if __BIG_ENDIAN__
-        if (((val >> 24) & 0xFF) == needle) return (unsigned char*)haystack;
-        if (((val >> 16) & 0xFF) == needle) return 1 + (unsigned char*)haystack;
-        if (((val >> 8) & 0xFF) == needle) return 2 + (unsigned char*)haystack;
-        if ((val & 0xFF) == needle) return 3 + (unsigned char*)haystack;
-#else
-        if ((val & 0xFF) == needle) return (unsigned char*)haystack;
-        if (((val >> 8) & 0xFF) == needle) return 1 + (unsigned char*)haystack;
-        if (((val >> 16) & 0xFF) == needle) return 2 + (unsigned char*)haystack;
-        if (((val >> 24) & 0xFF) == needle) return 3 + (unsigned char*)haystack;
-#endif
-	
-        haystack += 4;
-    }
-    
-    while (suffixLength--) {
-        if (*haystack == needle) return (unsigned char*)haystack;
-        haystack++;
-    }
-    
-    return NULL;
-}
-
 unsigned char *HFFastMemchr(const unsigned char *haystack, unsigned char needle, size_t length) {
     if (length == 0) return NULL;
 #if defined(__i386__) || defined(__x86_64__)
