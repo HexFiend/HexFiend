556efb8f4c86138551b58e636a665ecb28fb4511
Mattias Wadman <mattias.wadman@gmail.com>
2020-06-29 06:33:56 +0200

    Add "Show in Template" feature (#251)
    Selects the deepest template node for a given position.

--- app/sources/HFBinaryTemplateRepresenter.m
+++ app/sources/HFBinaryTemplateRepresenter.m
@@ -49,16 +49,30 @@
     if (menu.numberOfItems > 0) {
         [menu addItem:[NSMenuItem separatorItem]];
     }
+    
     self.menuPosition = position;
-    NSString *title = [NSString stringWithFormat:NSLocalizedString(@"Anchor Template at Offset %lu", nil), (unsigned long)self.menuPosition];
-    NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:title action:@selector(anchorTemplatesAt:) keyEquivalent:@""];
-    menuItem.target = self;
-    menuItem.enabled = position < self.controller.contentsLength;
-    [menu addItem:menuItem];
+    
+    NSString *anchorTitle = [NSString stringWithFormat:NSLocalizedString(@"Anchor Template at Offset %lu", nil), (unsigned long)self.menuPosition];
+    NSMenuItem *anchorMenuItem = [[NSMenuItem alloc] initWithTitle:anchorTitle action:@selector(anchorTemplatesAt:) keyEquivalent:@""];
+    anchorMenuItem.target = self;
+    anchorMenuItem.enabled = position < self.controller.contentsLength;
+    [menu addItem:anchorMenuItem];
+    
+    if (self.viewController.hasTemplate) {
+        NSString *gotoInTemplateTitle = NSLocalizedString(@"Show in Template", nil);
+        NSMenuItem *gotoInTemplateMenuItem = [[NSMenuItem alloc] initWithTitle:gotoInTemplateTitle action:@selector(showInTemplateAt:) keyEquivalent:@""];
+        gotoInTemplateMenuItem.target = self;
+        gotoInTemplateMenuItem.enabled = position < self.controller.contentsLength;
+        [menu addItem:gotoInTemplateMenuItem];
+    }
 }
 
 - (void)anchorTemplatesAt:(id __unused)sender {
     [self.viewController anchorTo:self.menuPosition];
+}
+
+- (void)showInTemplateAt:(id __unused)sender {
+    [self.viewController showInTemplateAt:self.menuPosition];
 }
 
 @end
