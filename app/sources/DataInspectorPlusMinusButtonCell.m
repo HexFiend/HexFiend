5badd3570b2569f03609c3220dd3a520cc3df29d
Kevin Wojniak <kainjow@users.noreply.github.com>
2020-11-13 14:44:39 -0800

    Fix plus and minus buttons on Big Sur
    Fixes #285

--- app/sources/DataInspectorPlusMinusButtonCell.m
+++ app/sources/DataInspectorPlusMinusButtonCell.m
@@ -18,48 +18,21 @@
 
 - (void)drawDataInspectorTitleWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
     const BOOL isPlus = [[self title] isEqual:@"+"];
-    const unsigned char grayColor = 0x73;
-    const unsigned char alpha = 0xFF;
-    const unsigned short X = (alpha << 8) | grayColor;
-    const NSUInteger bytesPerPixel = sizeof X;
-    const unsigned short plusData[] = {
-        0,0,0,X,X,0,0,0,
-        0,0,0,X,X,0,0,0,
-        0,0,0,X,X,0,0,0,
-        X,X,X,X,X,X,X,X,
-        X,X,X,X,X,X,X,X,
-        0,0,0,X,X,0,0,0,
-        0,0,0,X,X,0,0,0,
-        0,0,0,X,X,0,0,0
-    };
-    
-    const unsigned short minusData[] = {
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        X,X,X,X,X,X,X,X,
-        X,X,X,X,X,X,X,X,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0
-    };
-    
-    const unsigned char * const bitmapData = (const unsigned char *)(isPlus ? plusData : minusData);
-    
-    NSInteger width = 8, height = 8;
-    assert(width * height * bytesPerPixel == sizeof plusData);
-    assert(width * height * bytesPerPixel == sizeof minusData);
-    NSRect bitmapRect = NSMakeRect(NSMidX(cellFrame) - width/2, NSMidY(cellFrame) - height/2, width, height);
-    bitmapRect = [controlView centerScanRect:bitmapRect];
-    
-    CGColorSpaceRef space = CGColorSpaceCreateWithName(kCGColorSpaceGenericGray);
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, bitmapData, width * height * bytesPerPixel, NULL);
-    CGImageRef image = CGImageCreate(width, height, CHAR_BIT, bytesPerPixel * CHAR_BIT, bytesPerPixel * width, space, (CGBitmapInfo)kCGImageAlphaPremultipliedLast, provider, NULL, YES, kCGRenderingIntentDefault);
-    CGDataProviderRelease(provider);
-    CGColorSpaceRelease(space);
-    [[NSGraphicsContext currentContext] setCompositingOperation:NSCompositingOperationSourceOver];
-    CGContextDrawImage(HFGraphicsGetCurrentContext(), *(CGRect *)&bitmapRect, image);
-    CGImageRelease(image);
+    const CGFloat thickness = 2;
+    const CGFloat size = 8;
+    const NSRect horizontalBarRect = NSMakeRect(
+        cellFrame.origin.x + floor((cellFrame.size.width - size) / 2),
+        cellFrame.origin.y + floor((cellFrame.size.height - thickness) / 2),
+        size, thickness);
+    const NSRect verticalBarRect = NSMakeRect(
+        cellFrame.origin.x + floor((cellFrame.size.width - thickness) / 2),
+        cellFrame.origin.y + floor((cellFrame.size.height - size) / 2),
+        thickness, size);
+    [[NSColor colorWithCalibratedWhite:0.45 alpha:1.0] setFill];
+    [NSBezierPath fillRect:horizontalBarRect];
+    if (isPlus) {
+        [NSBezierPath fillRect:verticalBarRect];
+    }
 }
 
 - (NSRect)drawTitle:(NSAttributedString*)title withFrame:(NSRect)frame inView:(NSView*)controlView {
