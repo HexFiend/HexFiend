65a0ddfc38dd8f43f1e4663c69f57dfd23c7b591
Foster Brereton <fbrereto@adobe.com>
2021-07-12 17:20:04 -0700

    Starting to work on Exif support

--- templates/Images/PNG.tcl
+++ templates/Images/PNG.tcl
@@ -43,18 +43,205 @@ proc ChunkPLTE {length} {
     return $entry_count
 }
 
+proc ChunkgAMA {} {
+    set gamma [uint32 Gamma]
+    set derived_gamma [expr 100000 / $gamma]
+    sectionvalue "Gamma: $derived_gamma"
+}
+
+proc ChunksRGB {} {
+    set intent [uint8 "Rendering Intent"]
+
+    switch $intent {
+        0 { set intent_str "Perceptual" }
+        1 { set intent_str "Relative Colorimetric" }
+        2 { set intent_str "Saturation" }
+        3 { set intent_str "Absolute Colorimetric" }
+        default { set intent_str "Unknown" }
+    }
+
+    sectionvalue "Intent: $intent_str"
+}
+
+proc IFDFieldByte {count} {
+    ascii $count "Value"
+}
+
+proc IFDFieldWord {count} {
+    uint16 "Word"
+}
+
+proc IFDFieldLong {count} {
+    uint32 "Long"
+}
+
+proc IFDFieldRational {count} {
+    uint32 "Numerator"
+    uint32 "Denominator"
+}
+
+proc IFDFieldFloat {count} {
+    float "Float"
+}
+
+proc IFDFieldDouble {count} {
+    double "Double"
+}
+
+proc ChunkeXIfIFDField {header_pos field_size component_count read_proc} {
+    set total_size [expr $component_count * $field_size]
+    set is_remote [expr $total_size > 4]
+    set read_pos 0
+
+    if {$is_remote} {
+        set offset [uint32 "Remote Offset"]
+        set read_pos [pos]
+        goto [expr $header_pos + $offset]
+    }
+
+    set result [$read_proc $component_count]
+
+    if {$is_remote} {
+        goto $read_pos
+    } else {
+        set leftovers [expr 4 - $total_size]
+        if {$leftovers > 0} {
+            bytes $leftovers "Extra ($leftovers bytes)"
+        }
+    }
+
+    return $result
+}
+
+proc ChunkeXIfIFDEntry {header_pos count} {
+    section "\[ $count \]" {
+        set tag_number [uint16 "Tag Number"]
+        set field_type [uint16 "Field Type"]
+        set component_count [uint32 "Component Count"]
+        switch $field_type {
+            1  { set tag_value [ChunkeXIfIFDField $header_pos 1 $component_count IFDFieldByte] }
+            2  { set tag_value [ChunkeXIfIFDField $header_pos 1 $component_count IFDFieldByte] }
+            3  { set tag_value [ChunkeXIfIFDField $header_pos 2 $component_count IFDFieldWord] }
+            4  { set tag_value [ChunkeXIfIFDField $header_pos 4 $component_count IFDFieldLong] }
+            5  { set tag_value [ChunkeXIfIFDField $header_pos 8 $component_count IFDFieldRational] }
+            6  { set tag_value [ChunkeXIfIFDField $header_pos 1 $component_count IFDFieldByte] }
+            7  { set tag_value [ChunkeXIfIFDField $header_pos 1 $component_count IFDFieldByte] }
+            8  { set tag_value [ChunkeXIfIFDField $header_pos 2 $component_count IFDFieldWord] }
+            9  { set tag_value [ChunkeXIfIFDField $header_pos 4 $component_count IFDFieldLong] }
+            10 { set tag_value [ChunkeXIfIFDField $header_pos 8 $component_count IFDFieldRational] }
+            11 { set tag_value [ChunkeXIfIFDField $header_pos 4 $component_count IFDFieldFloat] }
+            12 { set tag_value [ChunkeXIfIFDField $header_pos 8 $component_count IFDFieldDouble] }
+        }
+        sectionname $tag_number
+        sectionvalue $tag_value
+    }
+}
+
+proc ChunkeXIfIFD {header_pos} {
+    set count [uint16 "Entry Count"]
+    section "Entries" {
+        for {set i 0} {$i < $count} {incr i} {
+            ChunkeXIfIFDEntry $header_pos $i
+            if {$i == 3} {
+                # return
+            }
+        }
+        # sectionvalue "$count entries"
+    }
+    set next_offset [uint32 "Next IFD Offset"]
+    return
+    if {$next_offset != 0} {
+        set marker [pos]
+        move $next_offset
+        ChunkeXIfIFD $header_pos
+        goto $marker
+    }
+}
+
+proc ChunkeXIf {length} {
+    set header_pos [pos]
+    set header [uint16 Header]
+
+    if {$header == 0x4D4D} {
+        big_endian
+        entry "" "(Big Endian)" 2 [expr [pos] - 2]
+    } else {
+        little_endian
+        entry "" "(Little Endian)" 2 [expr [pos] - 2]
+    }
+
+    uint16 "Tag Mark"
+    set ifd_offset [uint32 "IFD Offset"]
+    if {$ifd_offset != 8} {
+        entry "" "WARNING: Unsupported IFD offset (but may be well-formed)"
+        return
+    }
+    ChunkeXIfIFD $header_pos
+}
+
+proc ChunkbKGD {length} {
+    if {$length == 1} {
+        uint8 "Color Index"
+    } elseif {$length == 2} {
+        uint16 "Gray"
+    } elseif {$length == 6} {
+        uint16 "Red"
+        uint16 "Green"
+        uint16 "Blue"
+    } else {
+        entry "" "WARNING: Unknown bKGD chunk length"
+    }
+
+    sectionvalue "Background Color"
+}
+
+proc ChunksBIT {length} {
+    if {$length == 1} {
+        uint8 "Gray"
+    } elseif {$length == 3} {
+        uint8 "Red"
+        uint8 "Green"
+        uint8 "Blue"
+    } elseif {$length == 2} {
+        uint8 "Gray"
+        uint8 "Alpha"
+    } elseif {$length == 4} {
+        uint8 "Red"
+        uint8 "Green"
+        uint8 "Blue"
+        uint8 "Alpha"
+    } else {
+        entry "" "WARNING: Unknown sBIT chunk length"
+    }
+
+    sectionvalue "Significant Bits"
+}
+
+proc ChunkcHRM {} {
+    uint32 "White Point x"
+    uint32 "White Point y"
+    uint32 "Red x"
+    uint32 "Red y"
+    uint32 "Green x"
+    uint32 "Green y"
+    uint32 "Blue x"
+    uint32 "Blue y"
+
+    sectionvalue "Chromaticities"
+}
+
 proc ChunkpHYs {} {
-    set ppux [uint32 "Pixels Per Unit X"];
-    set ppuy [uint32 "Pixels Per Unit Y"];
+    set ppux [uint32 "Pixels Per Unit X"]
+    set ppuy [uint32 "Pixels Per Unit Y"]
     set ratio [expr $ppux / $ppuy]
-    set unit [uint8 "Unit"];
+    set unit [uint8 "Unit"]
     if {$unit == 1} {
         # unit is the meter
-        set dpi [expr $ppux * 0.0254];
-        sectionvalue "Aspect Ratio $ratio; $dpi DPI"
+        set dpi [expr $ppux * 0.0254]
+        sectionvalue "Aspect Ratio: $ratio; DPI: $dpi"
     } else {
         # unit is unknown
-        sectionvalue "Aspect Ratio $ratio"
+        sectionvalue "Aspect Ratio: $ratio"
     }
 }
 
@@ -66,7 +253,18 @@ proc ChunkiTXt {length} {
     cstr "utf8" "Language Tag"
     cstr "utf8" "Translated Keyword"
     set leftovers [expr $length - ([pos] - $start)]
-    ascii $leftovers "Text";
+    ascii $leftovers "Text"
+    sectionvalue "\'$keyword\'"
+}
+
+proc ChunkzTXt {length} {
+    set start [pos]
+    set keyword [cstr "utf8" "Keyword"]
+    uint8 "Compression Method"
+    set leftovers [expr $length - ([pos] - $start)]
+    set deflated [bytes $leftovers]
+    set inflated [zlib_uncompress $deflated]
+    entry "Inflated Text" $inflated $leftovers [expr [pos] - $leftovers] 
     sectionvalue "\'$keyword\'"
 }
 
@@ -84,8 +282,23 @@ while {![end]} {
             sectionvalue "Image Data"
         } elseif {$type == "iTXt"} {
             ChunkiTXt $length
+        } elseif {$type == "zTXt"} {
+            ChunkzTXt $length
         } elseif {$type == "pHYs"} {
             ChunkpHYs
+        } elseif {$type == "gAMA"} {
+            ChunkgAMA
+        } elseif {$type == "sRGB"} {
+            ChunksRGB
+        } elseif {$type == "cHRM"} {
+            ChunkcHRM
+        } elseif {$type == "sBIT"} {
+            ChunksBIT $length
+        } elseif {$type == "bKGD"} {
+            ChunkbKGD $length
+        } elseif {$type == "eXIf"} {
+            ChunkeXIf $length
+            return
         } else {
             if {$length > 0 } {
     			bytes $length "Raw Data"
