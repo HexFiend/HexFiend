5fe971b614d0d7237e65c123b4031d27e39d11a8
theLMGN <me@foxt.dev>
2022-06-08 02:27:14 +0100

    [ICO.tcl] fix 256x256 images

--- templates/Images/ICO.tcl
+++ templates/Images/ICO.tcl
@@ -1,22 +1,42 @@
 little_endian
 requires 0 "00 00"
+
+# stolen from TIFF.tcl
+proc backwardEntry {label value length} {
+  move -$length
+  entry $label $value $length
+  move $length
+}
+
+
 section "Header" {
 	uint16 "Reserved"
 	set type [ uint16 ]
+
+
 	if { $type == 1 }  {
-		entry "Type" "Icon"
+		backwardEntry "Type" "Icon" 2 
 	} elseif { $type == 2 } {
-		entry "Type" "Cursor"
+		backwardEntry "Type" "Cursor" 2
 	} else {
-		entry "Type" $type
+		backwardEntry "Type" $type 2
 	}
 	
 	set imagecount [ uint16 "Image count" ]
 }
 for { set i 0 } { $i < $imagecount } { incr i } {
 	section "Image $i" {
-		uint8 "Width"
-		uint8 "Height"
+		set w [ uint8 ]
+		if { $w == 0 } {
+			set w 256
+		}
+		backwardEntry "Width" $w 1
+		set h [ uint8 ]
+		if { $h == 0 } {
+			set h 256
+		}
+		backwardEntry "Height" $h 1
+
 		uint8 "Color palette size"
 		uint8 "Reserved"
 		if { $type == 1 } {
