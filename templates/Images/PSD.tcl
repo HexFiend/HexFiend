b8b271dfeb4b44b1e4306501e30be7f52adefee7
LogicalJoe <lj@LogicalJoe.com>
2023-06-10 02:41:57 -0400

    Fix PSD template empty data read (#387)
    some PSD files are generated with empty data sections, especially from Photopea.  This checks for and displays them properly.

--- templates/Images/PSD.tcl
+++ templates/Images/PSD.tcl
@@ -129,7 +129,11 @@ if {$imrs_len > 0} {
 				pascalString "Name" 2
 				set data_len [uint32 "Data length"]
 				set data_len_padded [padto2 $data_len]
-				bytes $data_len_padded "Data"
+				if {$data_len_padded > 0} {
+					bytes $data_len_padded "Data"
+				} else {
+					entry "Data" "empty"
+				}
 				set i [incr i]
 			}
 		}
@@ -246,7 +250,12 @@ if {$lars_len > 0} {
 											}
 											ascii 4 "Key"
 											set data_len [uint32 "Length"]
-											bytes [padto2 $data_len] "Data"
+											set data_len_padded [padto2 $data_len]
+											if {$data_len_padded > 0} {
+												bytes $data_len_padded "Data"
+											} else {
+												entry "Data" "empty"
+											}
 										}
 									}
 								}
