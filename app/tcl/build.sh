8aaec6cfd9778ca483523cdf7d34e0ff2b1a0a43
Kevin Wojniak <kainjow@users.noreply.github.com>
2023-07-10 01:27:11 -0700

    Remove files that cause code signing errors

--- app/tcl/build.sh
+++ app/tcl/build.sh
@@ -4,15 +4,30 @@ VERSION=8.6.13
 URL=http://prdownloads.sourceforge.net/tcl/tcl${VERSION}-src.tar.gz
 FILENAME=tcl.tar.gz
 DIR=tcl
+FRAMEWORK_DIR=build/tcl/Tcl.framework
 
+# Download the archive if it doesn't exist.
+# The SourceForge url downloads automatically if it determines the user agent isn't a browser.
+# nscurl automatically handles redirects by default.
 if [ ! -f "${FILENAME}" ]; then
     nscurl ${URL} -o "${FILENAME}"
-    rm -rf "${DIR}"
-    mkdir "${DIR}"
-    tar -xvf "${FILENAME}" -C "${DIR}" --strip-components=1
 fi
 
-cd "${DIR}/macosx"
+# Clear out any existing expanded folder and extract the archive.
+rm -rf "${DIR}"
+mkdir "${DIR}"
+tar -xvf "${FILENAME}" -C "${DIR}" --strip-components=1
+
+# Build
+pushd "${DIR}/macosx"
 ./configure
 NCPU=$(sysctl -n hw.ncpu)
 make embedded -j ${NCPU}
+popd
+
+# Remove files that cause code signing errors. These don't appear to be needed.
+pushd "${FRAMEWORK_DIR}/Versions/Current"
+rm libtclstub8.6.a
+rm tclConfig.sh
+rm tclooConfig.sh
+popd
