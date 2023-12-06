437435340dbb800d5c6c793ceefc558d0148733d
Kevin Wojniak <kainjow@users.noreply.github.com>
2023-12-06 00:18:18 -0800

    More uint32 tests

--- Hex Fiend Tests/HFTclTemplateControllerTests.swift
+++ Hex Fiend Tests/HFTclTemplateControllerTests.swift
@@ -14,8 +14,30 @@ extension HFRange: Equatable {
     }
 }
 
+private struct Node: Equatable {
+    let label: String?
+    let value: String?
+    let isGroup: Bool
+    let range: HFRange
+    
+    init(label: String?, value: String?, isGroup: Bool, range: HFRange) {
+        self.label = label
+        self.value = value
+        self.isGroup = isGroup
+        self.range = range
+    }
+    
+    init(_ label: String, _ value: String?, isGroup: Bool = false, _ range: HFRange) {
+        self.init(label: label, value: value, isGroup: isGroup, range: range)
+    }
+    
+    init(_ label: String, _ value: String?, isGroup: Bool = false, _ range: (location: Int, length: Int)) {
+        self.init(label: label, value: value, isGroup: isGroup, range: .init(location: UInt64(range.location), length: UInt64(range.length)))
+    }
+}
+
 final class HFTclTemplateControllerTests: XCTestCase {
-    private func evaluate(_ hexBytes: String, _ tclScript: String) throws -> (error: NSString?, root: HFTemplateNode) {
+    private func evaluate(_ hexBytes: String,_ tclScript: String) throws -> (error: NSString?, root: HFTemplateNode, nodes: [Node]) {
         let uuid = UUID().uuidString
         let url = NSURL.fileURL(withPath: NSTemporaryDirectory()).appendingPathComponent("\(uuid).tcl")
         try tclScript.write(to: url, atomically: true, encoding: .utf8)
@@ -34,18 +56,54 @@ final class HFTclTemplateControllerTests: XCTestCase {
         var error: NSString?
         let template = HFTclTemplateController()
         let root = template.evaluateScript(url.path, for: controller, error: &error)
-        return (error, root)
+        let rootChildren = try XCTUnwrap(root.children as? [HFTemplateNode])
+        let nodes: [Node] = rootChildren.map { node in
+                .init(label: node.label, value: node.value, isGroup: node.isGroup, range: node.range)
+        }
+        return (error, root, nodes)
     }
     
-    func testUInt32WithLabel() throws {
-        let result = try evaluate("DEADBEEF", "uint32 Magic")
-        XCTAssertNil(result.error)
-        XCTAssertTrue(result.root.isGroup)
-        XCTAssertEqual(result.root.children.count, 1)
-        let node = try XCTUnwrap(result.root.children[0] as? HFTemplateNode)
-        XCTAssertEqual(node.label, "Magic")
-        XCTAssertEqual(node.value, "4022250974")
-        XCTAssertEqual(node.range, HFRangeMake(0, 4))
-        XCTAssertFalse(node.isGroup)
+    func testUInt32() throws {
+        let script = """
+uint32 "Magic"
+"""
+        XCTAssertEqual(try evaluate("DEADBEEF", script).nodes,
+                       [.init("Magic", "4022250974", (0, 4))])
     }
+    
+    func testLittleEndianUInt32() throws {
+        let script = """
+little_endian
+uint32 "Magic"
+"""
+        XCTAssertEqual(try evaluate("DEADBEEF", script).nodes,
+                       [.init("Magic", "4022250974", (0, 4))])
+    }
+    
+    func testBigEndianUInt32() throws {
+        let script = """
+big_endian
+uint32 "Magic"
+"""
+        XCTAssertEqual(try evaluate("DEADBEEF", script).nodes,
+                       [.init("Magic", "3735928559", (0, 4))])
+    }
+    
+    func testUInt32Hex() throws {
+        let script = """
+uint32 -hex "Magic"
+"""
+        XCTAssertEqual(try evaluate("DEADBEEF", script).nodes,
+                       [.init("Magic", "0xEFBEADDE", (0, 4))])
+    }
+
+    func testUInt32NoLabelWithEntry() throws {
+        let script = """
+set magic [uint32]
+entry "Magic" $magic
+"""
+        XCTAssertEqual(try evaluate("DEADBEEF", script).nodes,
+                       [.init("Magic", "4022250974", (0, 0))])
+    }
+
 }
