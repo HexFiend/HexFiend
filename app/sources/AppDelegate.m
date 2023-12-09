dbebeb851cf4e0ee694a4ed659844ddc5b3152db
Kevin Wojniak <kainjow@users.noreply.github.com>
2023-12-09 07:50:30 -0800

    Disable Compare menu items for Compare

--- app/sources/AppDelegate.m
+++ app/sources/AppDelegate.m
@@ -274,9 +274,16 @@ static NSComparisonResult compareFontDisplayNames(NSFont *a, NSFont *b, void *un
 }
 
 - (BOOL)validateMenuItem:(NSMenuItem *)item {
-    SEL sel = [item action];
+    const SEL sel = [item action];
+    BaseDataDocument *document = NSDocumentController.sharedDocumentController.currentDocument;
+    const BOOL isDiffDocument = [document isKindOfClass:[DiffDocument class]];
+    if (isDiffDocument) {
+        if (sel == @selector(diffFrontDocuments:) ||
+            sel == @selector(diffFrontDocumentsByRange:)) {
+            return NO;
+        }
+    }
     if (sel == @selector(setFontFromMenuItem:)) {
-        BaseDataDocument *document = [[NSDocumentController sharedDocumentController] currentDocument];
         BOOL check = NO;
         if (document) {
             NSFont *font = [document font];
